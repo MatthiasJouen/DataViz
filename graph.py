2728c3f26cbedc12410d788ba56b9a8535c7b020
MatthiasJouen <matthias.jouen@edu.esiee.fr>
2020-11-10 09:37:29 +0100

    test hitogrammes

--- graph.py
+++ graph.py
@@ -12,7 +12,7 @@ def histo(wheres, weeks, param):
 	where = None
 	#################################
 
-	extract_data = [] # Data en fonction du Pays/Région/Département
+	extract_data = [[1,2,1,2,3,4]] # Data en fonction du Pays/Région/Département
 	
 	i = 0
 
@@ -62,7 +62,8 @@ def histo(wheres, weeks, param):
 
 
 			if len(weeks)>1:
-				plt.title('''HISTOGRAMME POUR L'ETAT DU(DE) '''+where+' AU '+weeks[position.index(element)])
+				plt.title('''HISTOGRAMME POUR L'ETAT DU(DE) '''+where+' AU ')
+				#+weeks[position.index(element)]
 			else:
 				plt.title('''HISTOGRAMME POUR L'ETAT DU(DE) '''+wheres[position.index(element)]+' AU '+week)
 
@@ -73,15 +74,16 @@ def histo(wheres, weeks, param):
 
 		else:
 
-			data = [extract_data[position.index(element)].cat1, extract_data[position.index(element)].cat2, extract_data[position.index(element)].cat3, extract_data[position.index(element)].cat4, extract_data[position.index(element)].cat5]
+			data = [extract_data[position.index(element)]]
+			# .cat1, extract_data[position.index(element)].cat2, extract_data[position.index(element)].cat3, extract_data[position.index(element)].cat4, extract_data[position.index(element)].cat5
 			x = [[0]*52, [0]*52, [0]*52, [0]*52, [0]*52]
 
 			
 
-			while i<min(len(data[l]) for l in range(len(data))):
-				for j in range(5):
-					x[j][(i%53)-1] += data[j][i]
-				i +=1
+			# while i<min(len(data[l]) for l in range(len(data))):
+			# 	for j in range(5):
+			# 		x[j][(i%53)-1] += data[j][i]
+			# 	i +=1
 
 
 
@@ -92,11 +94,8 @@ def histo(wheres, weeks, param):
 			plt.xlabel("NOMBRE DE MORTS")
 			plt.ylabel("NOMBRE DE SEMAINES")
 
-
-			if len(weeks)>1:
-				plt.title('''HISTOGRAMME POUR L'ETAT DU(DE) '''+where+' AU '+weeks[position.index(element)])
-			else:
-				plt.title('''HISTOGRAMME POUR L'ETAT DU(DE) '''+wheres[position.index(element)]+' AU '+week)
+			plt.title('''HISTOGRAMME POUR L'ETAT DU(DE) '''+wheres[position.index(element)])
+			
 
 
 
@@ -105,4 +104,3 @@ def histo(wheres, weeks, param):
 
 			
 	plt.show()
-	plt.close()
--- main.py
+++ main.py
@@ -30,212 +30,215 @@ except ImportError as E:
 
 
 appel = i = conditions = 0
-year_list = state_list = []
+year_list = [2, 1,5]
+state_list = ['France', 'USA', 'Allemagne']
 
-f = tk.Tk()
-f.title('PROJET PYTHON E3')
-f.resizable(width=False, height=False)
+# f = tk.Tk()
+# f.title('PROJET PYTHON E3')
+# f.resizable(width=False, height=False)
 
-class Bouton:
-	"""
-		Bouton permettant de garder en mémoire le choix de l'utilisateur
-	"""
+histo(state_list, year_list, 1)
 
-	def __init__(self, pValeur=''):
-		if type(pValeur)==str:
-			self.valeur = pValeur
-			self.chose = False
-			self.bouton = None
-			self.width = 5
+# class Bouton:
+	# """
+	# 	Bouton permettant de garder en mémoire le choix de l'utilisateur
+	# """
 
-
-	def getchose(self):
-		return self.chose
-
-
-	def getstate(self):
-		return self.valeur
+	# def __init__(self, pValeur=''):
+	# 	if type(pValeur)==str:
+	# 		self.valeur = pValeur
+	# 		self.chose = False
+	# 		self.bouton = None
+	# 		self.width = 5
 
 
-	def selection(self):
-		conditions = 0
-		#suppress()
+	# def getchose(self):
+	# 	return self.chose
 
-		if self.chose:
-			self.chose = False
-		else:
-			self.chose = True
 
-		if self.valeur=='TESTER':
-			generate()
-		elif self.valeur=='QUITTER':
-			f.destroy()
+	# def getstate(self):
+	# 	return self.valeur
 
-		self.change_button()
+
+	# def selection(self):
+	# 	conditions = 0
+	# 	#suppress()
+
+	# 	if self.chose:
+	# 		self.chose = False
+	# 	else:
+	# 		self.chose = True
+
+	# 	if self.valeur=='TESTER':
+	# 		generate()
+	# 	elif self.valeur=='QUITTER':
+	# 		f.destroy()
+
+	# 	self.change_button()
 			
-		try:
-			int(self.valeur)
-		except Exception:
-			# mettre à jour
-			pass
+	# 	try:
+	# 		int(self.valeur)
+	# 	except Exception:
+	# 		# mettre à jour
+	# 		pass
 
 				
-	def config_width(self, pWidth):
-		self.bouton.config(width=pWidth)
+	# def config_width(self, pWidth):
+	# 	self.bouton.config(width=pWidth)
 
 
-	def change_button(self):
-		if self.chose:
-			self.bouton.config(bg='green', relief='flat')
-		else:
-			self.bouton.config(bg='grey', relief='raised')
+	# def change_button(self):
+	# 	if self.chose:
+	# 		self.bouton.config(bg='green', relief='flat')
+	# 	else:
+	# 		self.bouton.config(bg='grey', relief='raised')
 
 
-	def create(self):
-		self.bouton = tk.Button(f, width=self.width, bg='grey', relief='raised' ,text=self.valeur, command=self.selection)
+	# def create(self):
+	# 	self.bouton = tk.Button(f, width=self.width, bg='grey', relief='raised' ,text=self.valeur, command=self.selection)
 
 
-	def place(self, pI, pJ, par1, par2, par3):
-		if self.bouton != None:
-			if self.valeur in ('TESTER', 'QUITTER'):
-				self.bouton.grid(row=pI, columnspan=pJ, padx=par1, pady=par2, sticky=par3)
-			else:
-				self.bouton.grid(row=pI, column=pJ, padx=par1, pady=par2, sticky=par3)
+	# def place(self, pI, pJ, par1, par2, par3):
+	# 	if self.bouton != None:
+	# 		if self.valeur in ('TESTER', 'QUITTER'):
+	# 			self.bouton.grid(row=pI, columnspan=pJ, padx=par1, pady=par2, sticky=par3)
+	# 		else:
+	# 			self.bouton.grid(row=pI, column=pJ, padx=par1, pady=par2, sticky=par3)
 
 
-	def delete(self):
+	# def delete(self):
 
-		if self.bouton != None:
-			self.bouton.destroy()
+	# 	if self.bouton != None:
+	# 		self.bouton.destroy()
 
-def data():
+# def data():
 
-	string = ''
-	f3 = tk.Toplevel(f)
-
-
-
-	for state in state_list:
-
-		for year in year_list:
-
-			string += '\n\nETAT : ' + state
-			string += ''' pour l'année ''' + year + ' :\n\n'
-
-			stat = [[]] # Stats a partir de fonctions.py
-
-			string += ' -> MOYENNE : ' + str(stat[0][0]) + '\n'
-			string += ' -> MEDIANE : ' + str(stat[0][1]) + '\n'
-			string += ' -> ECART TYPE : ' + str(stat[0][2]) + '\n'
-			string += ' -> VARIANCE : ' + str(stat[0][3]) + '\n'
-
-
-	#chaine1 = Label(f3, text=string).grid(row=1, column=1, columnspan=3)
-
-def charger(param):
-	"""
-		Charge l'histogramme ou la map en fonction du choix de l'utilisateur
-	"""
-	if param==3:
-		data()
-
-	elif conditions==1 or param==0:
-		draw(state_list, year_list)
-
-	elif conditions==2:
-		histo(state_list, year_list, param)	
-
-def suppress(window):
-	"""
-		Ferme la fenetre passée en parametre
-	"""
-	try:
-		window.destroy()
-	except Exception:
-		pass
-
-
-def generate():
-
-	try:
-		##########################################################
-		# Création des itérables selon le choix de l'utilisateur #
-		##########################################################
-
-		state_list = []
-		year_list = []
-
-	except Exception:
-		tkMessageBox.showinfo('ERREUR : Interne', 'Un problème est survenu veuillez réessayer')
-		conditions = 0
-
-	else:
-		if len(state_list)==0 or len(year_list)==0:
-			tkMessageBox.showinfo('ERREUR : Données', 'Il manque une ou plusieurs données')
-			return
+# 	string = ''
+# 	f3 = tk.Toplevel(f)
 
 
 
-	if len(year_list)==1:
+# 	for state in state_list:
 
-		if len(state_list)>4:
-			conditions = 1
-		else:
-			conditions = 2
+# 		for year in year_list:
 
-	elif len(year_list)>1 and len(year_list)<=4:
+# 			string += '\n\nETAT : ' + state
+# 			string += ''' pour l'année ''' + year + ' :\n\n'
 
-		if len(state_list)>1:
-			conditions = 1
-		else:
-			conditions = 2
+# 			stat = [[]] # Stats a partir de fonctions.py
 
-	elif len(year_list)>4:
+# 			string += ' -> MOYENNE : ' + str(stat[0][0]) + '\n'
+# 			string += ' -> MEDIANE : ' + str(stat[0][1]) + '\n'
+# 			string += ' -> ECART TYPE : ' + str(stat[0][2]) + '\n'
+# 			string += ' -> VARIANCE : ' + str(stat[0][3]) + '\n'
 
-		tkMessageBox.showinfo('ERREUR : Années', '''Vous avez entré un trop grand nombre d'années''')
+
+# 	#chaine1 = Label(f3, text=string).grid(row=1, column=1, columnspan=3)
+
+# def charger(param):
+# 	"""
+# 		Charge l'histogramme ou la map en fonction du choix de l'utilisateur
+# 	"""
+# 	if param==3:
+# 		data()
+
+# 	elif conditions==1 or param==0:
+# 		draw(state_list, year_list)
+
+# 	elif conditions==2:
+# 		histo(state_list, year_list, param)	
+
+# def suppress(window):
+# 	"""
+# 		Ferme la fenetre passée en parametre
+# 	"""
+# 	try:
+# 		window.destroy()
+# 	except Exception:
+# 		pass
+
+
+# def generate():
+
+# 	try:
+# 		##########################################################
+# 		# Création des itérables selon le choix de l'utilisateur #
+# 		##########################################################
+
+# 		state_list = []
+# 		year_list = []
+
+# 	except Exception:
+# 		tkMessageBox.showinfo('ERREUR : Interne', 'Un problème est survenu veuillez réessayer')
+# 		conditions = 0
+
+# 	else:
+# 		if len(state_list)==0 or len(year_list)==0:
+# 			tkMessageBox.showinfo('ERREUR : Données', 'Il manque une ou plusieurs données')
+# 			return
+
+
+
+# 	if len(year_list)==1:
+
+# 		if len(state_list)>4:
+# 			conditions = 1
+# 		else:
+# 			conditions = 2
+
+# 	elif len(year_list)>1 and len(year_list)<=4:
+
+# 		if len(state_list)>1:
+# 			conditions = 1
+# 		else:
+# 			conditions = 2
+
+# 	elif len(year_list)>4:
+
+# 		tkMessageBox.showinfo('ERREUR : Années', '''Vous avez entré un trop grand nombre d'années''')
 		
-	else:
+# 	else:
 
-		tkMessageBox.showinfo('ERREUR : Années', '''Vous avez entré un trop grand nombre d'années''')
+# 		tkMessageBox.showinfo('ERREUR : Années', '''Vous avez entré un trop grand nombre d'années''')
 
 
 
-	year = ' '.join(year_list)
-	state = ' '.join(state_list)
+# 	year = ' '.join(year_list)
+# 	state = ' '.join(state_list)
 
-	f2 = tk.Toplevel(f)
-	f2.title('CHOIX')
+# 	f2 = tk.Toplevel(f)
+# 	f2.title('CHOIX')
 
-	str1 = '''\n\nTracer le(les) histogramme(s) de(s) état(s) : ''' + state + ''' pour l'année ''' + year + ' OPTION : TOTAL\n\n'
-	str3 = '''\n\nTracer la carte de(s) état(s) : de(s) état(s) : ''' + state + ''' pour l'année ''' + year + '\n\n'
-	str2 = '''\n\nTracer le(les) histogramme(s) de(s) état(s) : ''' + state + ''' pour l'année ''' + year + ' OPTION : MULTI-ÂGE\n\n'
-	str4 = '''\n\nAfficher les statistiques des données : ''' + state + ''' pour l'années ''' + year + '\n\n'
+# 	str1 = '''\n\nTracer le(les) histogramme(s) de(s) état(s) : ''' + state + ''' pour l'année ''' + year + ' OPTION : TOTAL\n\n'
+# 	str3 = '''\n\nTracer la carte de(s) état(s) : de(s) état(s) : ''' + state + ''' pour l'année ''' + year + '\n\n'
+# 	str2 = '''\n\nTracer le(les) histogramme(s) de(s) état(s) : ''' + state + ''' pour l'année ''' + year + ' OPTION : MULTI-ÂGE\n\n'
+# 	str4 = '''\n\nAfficher les statistiques des données : ''' + state + ''' pour l'années ''' + year + '\n\n'
 
-	#############################################################################
-	# Création des boutons de choix de sélection d'interfaces (histo, map, stat)#
-	#############################################################################
+# 	#############################################################################
+# 	# Création des boutons de choix de sélection d'interfaces (histo, map, stat)#
+# 	#############################################################################
 
-	while True:
+# 	while True:
 		
-		########################################################
-		# Création Itérable selon le nombre de lieux différents#
-		########################################################
-		liste_button = {}
+# 		########################################################
+# 		# Création Itérable selon le nombre de lieux différents#
+# 		########################################################
+# 		liste_button = {}
 		
-		i = 2
-		j = 1
+# 		i = 2
+# 		j = 1
 
-		for state, button in liste_button.items():
-			if j%8==0:
-				i+=1
-				j=1
-			button.create()
-			button.place(i, j, 0, 0, None)
-			j +=1
+# 		for state, button in liste_button.items():
+# 			if j%8==0:
+# 				i+=1
+# 				j=1
+# 			button.create()
+# 			button.place(i, j, 0, 0, None)
+# 			j +=1
 
 
 
-	f.mainloop()
+# f.mainloop()
 
 if __name__ == '__main__':
 	# Lancer programme principal
