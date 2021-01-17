98a3d20140a5921cd251298b7c12be7357e19ec3
unknown <helie.damien77@gmail.com>
2021-01-17 15:03:58 +0100

    prevent error getting data

--- src/fonctions.py
+++ src/fonctions.py
@@ -7,8 +7,12 @@ import json
 def get_data_with_url():
     #On recupere les donnees de l'api et on les retournes sous DataFrame
     url = 'https://coronavirusapi-france.now.sh/AllLiveData'
-    with urllib.request.urlopen(url) as url:
-        data = json.loads(url.read().decode())
-        df = pd.json_normalize(data)
-        dt = df.to_dict()
-        return pd.DataFrame.from_dict(dt['allLiveFranceData'][0])
+
+    try:
+        with urllib.request.urlopen(url) as url:
+            data = json.loads(url.read().decode())
+            df = pd.json_normalize(data)
+            dt = df.to_dict()
+            return pd.DataFrame.from_dict(dt['allLiveFranceData'][0])
+    except urllib.error.URLError:
+        print("Cant access {0}".format(url))
